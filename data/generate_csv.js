2ac166dfe735e9467a36f1e399ed757ca97f6634
jeffreyshen19 <jshen@andover.edu>
2017-10-17 01:07:13 -0400

    updated display

--- data/generate_csv.js
+++ data/generate_csv.js
@@ -14,10 +14,10 @@ function addLine(line){
 }
 
 function generateLine(json){
-  return json.district_code + "," + json.absolute_efficiency_gap + "," + json.state_efficiency_gap + "," + json.district_efficiency_gap + "," + json.absolute_compactness + "," + json.state_compactness + "," + json.country_compactness + "," + json.affiliation + "," + json.gerrymander_score + "," + json.rep;
+  return json.district_code + "," + json.absolute_efficiency_gap + "," + json.state_efficiency_gap + "," + json.district_efficiency_gap + "," + json.absolute_compactness + "," + json.state_compactness + "," + json.country_compactness + "," + json.affiliation + "," + json.gerrymander_score + "," + json.rep + "," + json.compactness_rank;
 }
 
-addLine("district_code,absolute_efficiency_gap,state_efficiency_gap,district_efficiency_gap,absolute_compactness,state_compactness,country_compactness,affiliation,gerrymander_score,rep");
+addLine("district_code,absolute_efficiency_gap,state_efficiency_gap,district_efficiency_gap,absolute_compactness,state_compactness,country_compactness,affiliation,gerrymander_score,rep,compactness_rank");
 
 var representatives_parsed = [];
 
@@ -41,6 +41,7 @@ curl.request({
   var country_avg = 0.0;
   var state_avgs = [];
   var state_districts = [];
+  var district_compactness_ranks = [];
   var current_state_i = -1;
   var csv = fs.readFileSync("compactness.csv").toString().split("\n");
 
@@ -51,10 +52,15 @@ curl.request({
       state_districts.push(0);
     }
 
+    district_compactness_ranks.push(parseFloat(csv[k + 1].split(",")[1]));
+
     state_avgs[current_state_i] += parseFloat(csv[k + 1].split(",")[1]);
     state_districts[current_state_i]++;
     country_avg += parseFloat(csv[k + 1].split(",")[1]);
   }
+
+  var sorted = district_compactness_ranks.slice().sort(function(a,b){return b-a;});
+  district_compactness_ranks = district_compactness_ranks.slice().map(function(v){ return sorted.indexOf(v)+1;});
 
   for(var l = 0; l < 50; l++) state_avgs[l] /= state_districts[l];
   country_avg /= district_codes.length;
@@ -78,7 +84,8 @@ curl.request({
       state_compactness: state_avgs[postal_codes.indexOf(district_codes[j].split("-")[0])],
       country_compactness: country_avg,
       affiliation: rep.party,
-      gerrymander_score: 0
+      gerrymander_score: 0,
+      compactness_rank: district_compactness_ranks[j]
     });
 
     addLine(line);
