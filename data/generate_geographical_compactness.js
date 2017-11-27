8b97930139aa246b0d0f19847313af120f242124
jeffreyshen19 <jshen@andover.edu>
2017-11-27 10:37:20 -0800

    geographical compactness for one district states fixed

--- data/generate_geographical_compactness.js
+++ data/generate_geographical_compactness.js
@@ -10,10 +10,13 @@ var content = "", geographical_compactness, geojson, area, perimeter;
 content += "district,compactness\n";
 
 for(var i = 0; i < 435; i++){
-  geojson = JSON.parse(fs.readFileSync("./raw/districts/" + district_codes[i] + "/shape.geojson").toString());
-  area = geojsonArea.geometry(geojson.geometry); //in meters squared
-  perimeter = turf.lineDistance(geojson.geometry, 'kilometers') * 1000; //meters
-  geographical_compactness = 4 * Math.PI * area / (perimeter * perimeter); //Using Polsby Popper method
+  if(district_codes[i].split("-")[1] === "0") geographical_compactness = 1;
+  else{
+    geojson = JSON.parse(fs.readFileSync("./raw/districts/" + district_codes[i] + "/shape.geojson").toString());
+    area = geojsonArea.geometry(geojson.geometry); //in meters squared
+    perimeter = turf.lineDistance(geojson.geometry, 'kilometers') * 1000; //meters
+    geographical_compactness = 4 * Math.PI * area / (perimeter * perimeter); //Using Polsby Popper method
+  }
 
   content += district_codes[i] + "," + geographical_compactness + "\n";
 }
